f354aed5f66f5d43482fa8717d633cd9662d4b4b
vincentgoatbot-v2 <vincentarmenion86@gmail.com>
2025-01-20 16:12:41 +0800

    Update offbot.js

--- scripts/cmds/offbot.js
+++ scripts/cmds/offbot.js
@@ -1,8 +1,8 @@
-module.exports = {
+ module.exports = {
   config: {
     name: "offbot",
     version: "1.0",
-    author: "Samir",
+    author: "armenion",
     countDown: 45,
     role: 0,
     shortDescription: "Turn off bot",
@@ -11,7 +11,7 @@ module.exports = {
     guide: "{p}{n}"
   },
   onStart: async function ({event, api}) {
-    const permission = ["61558651089120","61557606879019"];
+    const permission = ["100090775159086"];
   if (!permission.includes(event.senderID)) {
     api.sendMessage("You don't have enough permission to use this command. Only ʚɸɞ Aldéric ʚɸɞ can do it.", event.threadID, event.messageID);
     return;
